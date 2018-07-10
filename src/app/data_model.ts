0a9b5a02816a8a07157ecd2888f5b62b54455240
Sowmya <sowmya.modekurty@soch-inc.com>
2018-07-10 14:13:48 -0400

    Data Model

---
+++ src/app/data_model.ts
@@ -0,0 +1,83 @@
+/**
+ * New typescript file
+ */
+
+export class Solution {
+  solutionId: number;
+  // certDt:date;
+  certStadard: string;
+  // certRenewalDueDt:Date;
+  name: string;
+  versionNumber: string;
+  patches: string;
+  notes: string;
+  solutionType: number;
+  vendorId: number;
+  createdBy: string;
+  createdTs;
+  updatedBy: string;
+  updatedTs;
+  vendor :Vendor;
+  solutionTypeName: string;
+  labVendorId: number;
+  labVendorsDTO : LabVendors;
+  systemTypeDTO: SystemType;
+  hostingTypeDTO: HostingType;
+  certDocDTOs: Array<CertDocDTO> = [];
+}
+
+export class SystemType {
+  name: string;
+  systemTypeId: string
+}
+export class HostingType {
+  name: string;
+  hostingTypeId: string
+}
+
+export class LabVendors {
+  labVendorId: number;
+  name: string;
+  firstName: string;
+  lastName: string;
+  suffix: string;
+  emailId: string;
+  streetName: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  phoneNumber: string;
+}
+
+export class Vendor {
+  vendorId: number;
+  name: string;
+  createdBy: string;
+  vendorAddress: Address;
+  vendorContact: VendorContact;
+}
+
+export class Address {
+  addressId: number;
+  streetName: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
+export class VendorContact {
+  contactId: number;
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  suffix: string;
+  emailId: string;
+  phoneNumber: string;
+}
+
+export class CertDocDTO {
+   certDocId: number;
+  fileName: string;
+   fileLocation: string;
+  section:string;
+}
